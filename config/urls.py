bae66340fa611e0b0d262c9c5369a8438d3b8b29
eldarsaraj <eldar.sarajlic@gmail.com>
2026-01-06 17:36:43 -0500

    Serve MEDIA files on Heroku

--- config/urls.py
+++ config/urls.py
@@ -6,9 +6,9 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 """
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -16,5 +16,7 @@ urlpatterns = [
     path("", include("pages.urls")),
 ]
 
-# Serve media files (needed on Heroku when DEBUG=False)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Serve uploaded media files (works even when DEBUG=False)
+urlpatterns += [
+    re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+]
