8792f188a8a2a82da2636fbf1300fdf2e13024b3
eldarsaraj <eldar.sarajlic@gmail.com>
2026-02-17 17:19:15 -0500

    Fix admin: remove import from management command

--- diagnostic/admin.py
+++ diagnostic/admin.py
@@ -1,13 +1,9 @@
 # diagnostic/admin.py
-
 from django.contrib import admin
-from django.http import HttpResponse
-import csv
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from .models import DiagnosticLead
-from .management.commands.diagnostic_report import (
-    build_report_rows,
-)
 
 
 @admin.register(DiagnosticLead)
@@ -21,22 +17,13 @@ class DiagnosticLeadAdmin(admin.ModelAdmin):
 
     @admin.action(description="Download module report CSV (aggregated)")
     def download_module_report_csv(self, request, queryset):
-        return self._download_csv(map_version="v0_1", fmt="module")
+        url = reverse("diagnostic_admin_report_csv") + "?map_version=v0_1&format=module"
+        return HttpResponseRedirect(url)
 
     @admin.action(description="Download dimension report CSV (aggregated)")
     def download_dimension_report_csv(self, request, queryset):
-        return self._download_csv(map_version="v0_1", fmt="dimension")
-
-    def _download_csv(self, map_version: str, fmt: str):
-        rows, fieldnames = build_report_rows(map_version=map_version, agg=fmt)
-
-        response = HttpResponse(content_type="text/csv")
-        response["Content-Disposition"] = (
-            f'attachment; filename="diagnostic_{fmt}_report.csv"'
+        url = (
+            reverse("diagnostic_admin_report_csv")
+            + "?map_version=v0_1&format=dimension"
         )
-
-        writer = csv.DictWriter(response, fieldnames=fieldnames)
-        writer.writeheader()
-        writer.writerows(rows)
-
-        return response
+        return HttpResponseRedirect(url)
