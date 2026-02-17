dca19c277114d468e5ab26b6158b9aea23f779b2
eldarsaraj <eldar.sarajlic@gmail.com>
2026-02-17 12:20:58 -0500

    Fix admin 500: remove distinct(email) from DiagnosticLead admin

--- diagnostic/admin.py
+++ diagnostic/admin.py
@@ -1,8 +1,5 @@
 # diagnostic/admin.py
 from django.contrib import admin
-from django.db.models import F, Window
-from django.db.models.functions import RowNumber
-
 from .models import DiagnosticLead
 
 
@@ -12,17 +9,3 @@ class DiagnosticLeadAdmin(admin.ModelAdmin):
     list_filter = ("version", "created_at")
     search_fields = ("email", "full_name", "organization")
     ordering = ("-created_at",)
-
-    def get_queryset(self, request):
-        qs = super().get_queryset(request)
-
-        # Latest lead per email without DISTINCT ON (Postgres-safe in admin/paginator)
-        qs = qs.annotate(
-            rn=Window(
-                expression=RowNumber(),
-                partition_by=[F("email")],
-                order_by=F("created_at").desc(),
-            )
-        ).filter(rn=1)
-
-        return qs
